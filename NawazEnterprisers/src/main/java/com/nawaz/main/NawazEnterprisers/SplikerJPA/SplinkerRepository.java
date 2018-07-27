892a2821fc4bd0131242a585fef82cfaa20fe8cc
SHANAWAZ KHAN <SHANAWAZ KHAN@192.168.0.9>
2018-07-27 11:01:06 +0530

    i am placing my project in jit repository

---
+++ NawazEnterprisers/src/main/java/com/nawaz/main/NawazEnterprisers/SplikerJPA/SplinkerRepository.java
@@ -0,0 +1,11 @@
+package com.nawaz.main.NawazEnterprisers.SplikerJPA;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface SplinkerRepository  extends JpaRepository<Splinkers, Integer>{
+
+	
+
+}
